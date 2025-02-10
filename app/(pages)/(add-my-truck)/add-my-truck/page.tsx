e3f26c1edca7e5e748308e63c3ba64be78c17121
SujonHasan <sujonhasan171@gmail.com>
2025-02-10 17:21:37 +0600

    add my truck ymm added

--- app/(pages)/(add-my-truck)/add-my-truck/page.tsx
+++ app/(pages)/(add-my-truck)/add-my-truck/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+
 import Container from "@/app/ui/container/container";
+import useAuth from "../../_hooks/useAuth";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import AddMyTruckYMMFilters from "./_components/add-my-truck-ymm-filter";
 
 export default function Page() {
+
+    const {user} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!user) {
+            router.push('/login');
+        }
+    }, [user, router]);
+
     return (
         <Container>
-            <div>Add My Truck</div>
+            <div className="w-full">
+                <div className="w-full pt-3 pb-10">
+                    <h3 className="w-full text-center text-2xl font-medium" >Vehicle</h3>
+                </div>
+                <AddMyTruckYMMFilters />
+            </div>
         </Container>
     )
 }
