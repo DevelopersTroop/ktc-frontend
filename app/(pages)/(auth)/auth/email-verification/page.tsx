6c1d6d26489f52bf618ee4db4a9ca62e512cca02
SujonHasan <sujonhasan171@gmail.com>
2025-04-06 09:05:55 +0600

    auth update

--- app/(pages)/(auth)/auth/email-verification/page.tsx
+++ app/(pages)/(auth)/auth/email-verification/page.tsx
@@ -3,11 +3,14 @@
 import { useEffect, useState } from 'react';
 import {  useRouter, useSearchParams } from 'next/navigation';
 import { apiBaseUrl } from '@/app/utils/api';
+import Link from 'next/link';
 
 const EmailVerification = () => {
     const searchParams = useSearchParams();
+    const [sucessMessage, setSuccessMessage] = useState('') // State to store success messages
     const router = useRouter();
     const [verificationStarted, setVerificationStarted] = useState(false);
+    const [timer, setTimer] = useState(3) // Countdown timer for redirecting to the login page
 
     useEffect(() => {
         const verifyEmail = async () => {
@@ -22,17 +25,21 @@ const EmailVerification = () => {
                     body: JSON.stringify({ token, email }),
                 });
                 const result = await response.json();
+
+                // Handle successful email verification
                 if (result.statusCode === 200) {
-                    router.push("/");
+                    setSuccessMessage('Email verified successfully')
                 } else {
                     if(result.errors[0]?.message === "Email already verified"){
-                        router.push("/");
+                        setSuccessMessage('Email already verified');
                     }else {
                         router.push("/auth/email-verification-failed");
                     }
                 }
             } catch (error) {
                 router.push("/auth/email-verification-failed");
+            }finally {
+                setVerificationStarted(false);
             }
         };
         if (searchParams.get('token') && !verificationStarted) {
@@ -41,9 +48,43 @@ const EmailVerification = () => {
         }
     }, []);
 
+    // Effect to handle redirection to the login page after successful verification
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+        let intervalTimer: NodeJS.Timeout
+        if (sucessMessage) {
+            // Redirect to the login page after 3 seconds
+            timer = setTimeout(() => {
+                router.push('/login')
+            }, 3000);
+            // Update the countdown timer every second
+            intervalTimer = setInterval(() => {
+                setTimer(prev => prev - 1)
+            }, 1000)
+            // Cleanup timers when the component unmounts or the effect re-runs
+            return () => {
+                clearInterval(intervalTimer)
+                clearTimeout(timer)
+            }
+        }
+    }, [sucessMessage, router])
+
     return (
         <div className='py-56 text-center text-3xl'>
-            Please wait...
+            {
+                verificationStarted ? 'Please wait...' : sucessMessage.length ? (
+                    <div>
+                        <div className='text-center text-3xl text-primary'>
+                            {sucessMessage}
+                        </div>
+                        <Link href={'/login'} className='text-center text-lg'>
+                            Redirecting to login page in <span className='text-primary'>
+                                {timer}
+                            </span>
+                        </Link>
+                    </div>
+                ) : 'Something Went Wrong'
+            }
         </div>
     );
 };
