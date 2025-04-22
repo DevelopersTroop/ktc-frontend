8ab54b650a6ff0bca89695b008527cc4ef74af8d
Jahid Hiron <jahidul@ewwfl.com>
2025-04-22 13:07:40 +0600

    ISSUE: user create role issue fixed

--- app/(pages)/(auth)/register/page.tsx
+++ app/(pages)/(auth)/register/page.tsx
@@ -7,7 +7,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
-import { setAccessToken, setRefreshToken, setUserDetails } from "@/app/globalRedux/features/user/user-slice";
+import {
+  setAccessToken,
+  setRefreshToken,
+  setUserDetails,
+} from "@/app/globalRedux/features/user/user-slice";
 import { userRegister } from "./register";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 
@@ -45,37 +49,46 @@ const Page = () => {
   };
 
   return (
-    <div className="px-4 pt-2 md:p-0" style={banner}>
-      <div className="w-full md:h-full flex flex-col md:flex-row py-8 px-4 md:p-0 bg-gray-400 bg-opacity-70 rounded-md md:bg-transparent">
-        <div className="w-full md:w-[60%] text-white md:pt-2">
-          <h1 className="text-6xl text-center uppercase md:block hidden">
+    <div className='px-4 pt-2 md:p-0' style={banner}>
+      <div className='w-full md:h-full flex flex-col md:flex-row py-8 px-4 md:p-0 bg-gray-400 bg-opacity-70 rounded-md md:bg-transparent'>
+        <div className='w-full md:w-[60%] text-white md:pt-2'>
+          <h1 className='text-6xl text-center uppercase md:block hidden'>
             Get Access
           </h1>
-          <h1 className="text-2xl text-center uppercase md:hidden block">
+          <h1 className='text-2xl text-center uppercase md:hidden block'>
             Welcome Back
           </h1>
-          <div className="w-full h-2 bg-primary"></div>
+          <div className='w-full h-2 bg-primary'></div>
         </div>
-        <div className="md:bg-gray-400 md:bg-opacity-90 w-full md:w-[40%] flex justify-center items-center">
-          <div className="w-full shadow-[0_1px_3px_0_rgba(0,0,0,0.09)] pt-2 md:px-12 md:py-20">
-            <h2 className="text-2xl font-bold pb-4 border-b border-gray-200 text-white uppercase">
+        <div className='md:bg-gray-400 md:bg-opacity-90 w-full md:w-[40%] flex justify-center items-center'>
+          <div className='w-full shadow-[0_1px_3px_0_rgba(0,0,0,0.09)] pt-2 md:px-12 md:py-20'>
+            <h2 className='text-2xl font-bold pb-4 border-b border-gray-200 text-white uppercase'>
               Register
             </h2>
 
             {errors.length > 0 &&
               errors.map((error) => (
-                <Alert variant="destructive" key={error.message} className="mt-4">
+                <Alert
+                  variant='destructive'
+                  key={error.message}
+                  className='mt-4'
+                >
                   <AlertDescription>{error.message}</AlertDescription>
                 </Alert>
               ))}
             {success.isSuccess && (
-              <Alert className="mt-4">
+              <Alert className='mt-4'>
                 <AlertDescription>{success.message}</AlertDescription>
               </Alert>
             )}
 
             <Formik
-              initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+              initialValues={{
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+              }}
               validationSchema={validationSchema}
               onSubmit={(values) => {
                 setIsLoadingRegister(true);
@@ -84,14 +97,15 @@ const Page = () => {
                   lastName: values.lastName,
                   email: values.email,
                   password: values.password,
-                  role: 2,
+                  role: "customer",
                 })
                   .then((response) => {
                     const { statusCode, data, errors } = response;
                     if (data && statusCode === 201) {
                       setSuccess({
                         isSuccess: true,
-                        message: "Please check your email to verify your account",
+                        message:
+                          "Please check your email to verify your account",
                       });
                       setErrors([]);
                       // dispatch(setAccessToken({ accessToken: data.accessToken }));
@@ -109,13 +123,16 @@ const Page = () => {
               }}
             >
               {({ errors: formErrors, touched }) => (
-                <Form className="mt-6 space-y-4">
-                  <div className="grid grid-cols-2 gap-4">
+                <Form className='mt-6 space-y-4'>
+                  <div className='grid grid-cols-2 gap-4'>
                     <div>
-                      <label className="block font-semibold mb-1 text-white" htmlFor="firstName">
+                      <label
+                        className='block font-semibold mb-1 text-white'
+                        htmlFor='firstName'
+                      >
                         First Name
                       </label>
-                      <Field name="firstName">
+                      <Field name='firstName'>
                         {({ field }: any) => (
                           <Input
                             {...field}
@@ -128,16 +145,19 @@ const Page = () => {
                         )}
                       </Field>
                       {formErrors.firstName && touched.firstName && (
-                        <p className="mt-1 text-sm text-red-500">
+                        <p className='mt-1 text-sm text-red-500'>
                           {formErrors.firstName}
                         </p>
                       )}
                     </div>
                     <div>
-                      <label className="block font-semibold mb-1 text-white" htmlFor="lastName">
+                      <label
+                        className='block font-semibold mb-1 text-white'
+                        htmlFor='lastName'
+                      >
                         Last Name
                       </label>
-                      <Field name="lastName">
+                      <Field name='lastName'>
                         {({ field }: any) => (
                           <Input
                             {...field}
@@ -150,7 +170,7 @@ const Page = () => {
                         )}
                       </Field>
                       {formErrors.lastName && touched.lastName && (
-                        <p className="mt-1 text-sm text-red-500">
+                        <p className='mt-1 text-sm text-red-500'>
                           {formErrors.lastName}
                         </p>
                       )}
@@ -158,16 +178,16 @@ const Page = () => {
                   </div>
                   <div>
                     <label
-                      className="block font-semibold mb-1 text-white"
-                      htmlFor="email"
+                      className='block font-semibold mb-1 text-white'
+                      htmlFor='email'
                     >
                       Email address
                     </label>
-                    <Field name="email">
+                    <Field name='email'>
                       {({ field }: any) => (
                         <Input
                           {...field}
-                          type="email"
+                          type='email'
                           className={`bg-white ${
                             formErrors.email && touched.email
                               ? "border-red-500"
@@ -177,7 +197,7 @@ const Page = () => {
                       )}
                     </Field>
                     {formErrors.email && touched.email && (
-                      <p className="mt-1 text-sm text-red-500">
+                      <p className='mt-1 text-sm text-red-500'>
                         {formErrors.email}
                       </p>
                     )}
@@ -185,16 +205,16 @@ const Page = () => {
 
                   <div>
                     <label
-                      className="block font-semibold mb-1 text-white "
-                      htmlFor="password"
+                      className='block font-semibold mb-1 text-white '
+                      htmlFor='password'
                     >
                       Password
                     </label>
-                    <Field name="password">
+                    <Field name='password'>
                       {({ field }: any) => (
                         <Input
                           {...field}
-                          type="password"
+                          type='password'
                           className={`bg-white ${
                             formErrors.password && touched.password
                               ? "border-red-500"
@@ -204,14 +224,14 @@ const Page = () => {
                       )}
                     </Field>
                     {formErrors.password && touched.password && (
-                      <p className="mt-1 text-sm text-red-500">
+                      <p className='mt-1 text-sm text-red-500'>
                         {formErrors.password}
                       </p>
                     )}
                   </div>
                   <Button
-                    type="submit"
-                    className="w-full uppercase bg-primary"
+                    type='submit'
+                    className='w-full uppercase bg-primary'
                     disabled={isLoadingRegister}
                   >
                     {isLoadingRegister ? "Please wait..." : "Create Account"}
@@ -219,17 +239,17 @@ const Page = () => {
                 </Form>
               )}
             </Formik>
-            <div className="flex my-4 justify-end items-center gap-1">
-              <div className="w-full">
-                <hr className="border-t border-gray-200" />
+            <div className='flex my-4 justify-end items-center gap-1'>
+              <div className='w-full'>
+                <hr className='border-t border-gray-200' />
               </div>
-              <p className="text-sm text-nowrap text-white">
-                <Link href="/login" className=" hover:underline">
+              <p className='text-sm text-nowrap text-white'>
+                <Link href='/login' className=' hover:underline'>
                   Already have an account?
                 </Link>
               </p>
-              <div className="w-full">
-                <hr className="border-t border-gray-200" />
+              <div className='w-full'>
+                <hr className='border-t border-gray-200' />
               </div>
             </div>
           </div>
