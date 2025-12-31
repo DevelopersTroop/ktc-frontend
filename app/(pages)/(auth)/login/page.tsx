c2f5f440dfa00416df27eb00e4f7178955649051
Jahid Hiron <jahidul@ewwfl.com>
2025-12-31 16:34:46 +0600

    ISSUE: deploy issue

--- app/(pages)/(auth)/login/page.tsx
+++ app/(pages)/(auth)/login/page.tsx
@@ -206,8 +206,8 @@ const Page = () => {
               )}
             </Formik>
             <div className='flex gap-5 items-center justify-center'>
-              <GoogleAuth />
               <FacebookAuth />
+              <GoogleAuth />
             </div>
             <div className='my-4 flex items-center justify-end gap-1'>
               <div className='w-full'>
